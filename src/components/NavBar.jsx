5a5bd252d3bb984432ff9d8132382e1c10899faf
ArtiWebDev <arti.ramanovich@gmail.com.com>
2025-06-15 10:37:47 +0200

    fix issues

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ import profileAvatar from '../assets/avatar.png'
 import { useState } from 'react'
 import { useAuth } from '../context/AuthContext'
 
-import Logo from '../assets/logo.png'
+import Logo from '../assets/Logo.png'
 
 const NavBar = () => {
   const [isAvatarMenuOpen, setIsAvatarMenuOpen] = useState(false)
