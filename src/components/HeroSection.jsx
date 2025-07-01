08f4edbdfa5027e6ad17794dd67a1ba00af68b3b
ArtiWebDev <168907668+ArtiWebDev@users.noreply.github.com>
2025-07-01 08:22:55 +0200

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -34,6 +34,7 @@ const HeroSection = () => {
           <div className="mt-4">
             <button
               type="button"
+                onclick="location.href='https://github.com/ArtiWebDev/VinylWebStoreFullV2'"
               class="py-2 px-4 flex w-60 justify-center items-center  bg-gray-600 hover:bg-gray-700 focus:ring-gray-500 focus:ring-offset-gray-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
             >
               <svg
